306c3cb5444818a2417b374a8a8aa4bf70e459af
TheJeds <diegogalaxy15@gmail.com>
2022-05-12 15:41:00 -0500

    show buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
+import Button from './components/Button';
 import LogoTheJeds from './images/Logo-TheJeds.png'
 
 function App() {
+  
+  const handleClick = () => {
+    console.log('Click');
+  }
+
+  const resetCounter = () => {
+    console.log('Reset');
+  }
+
   return (
     <div className="App">
       <div className="container-TheJeds-logo">
         <img className="TheJeds-logo" src={LogoTheJeds} alt="Logo TheJeds" />
       </div>
       <div className='container-main'>
-        
+        <Button text='Click' isButtonClick={true} handleClick={handleClick} />
+        <Button text='Reset' isButtonClick={false} handleClick={resetCounter} />
       </div>
     </div>
   );
