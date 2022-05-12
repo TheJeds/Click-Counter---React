467a3cd029410d5cc375464972635e731dddb8bb
TheJeds <diegogalaxy15@gmail.com>
2022-05-12 14:46:28 -0500

    add css and button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import LogoTheJeds from './images/Logo-TheJeds.png'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container-TheJeds-logo">
+        <img className="TheJeds-logo" src={LogoTheJeds} alt="Logo TheJeds" />
+      </div>
+      <div className='container-main'>
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button({ text, isButtonClick, handleClick }) {
+    return (
+        <button className={ isButtonClick ? 'button-click' : 'button-restart' } onClick={ handleClick } >
+            {text}
+        </button>
+    )
+}
+
+export default Button;
